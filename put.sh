c2bb96f57c79ae3ff1a91be6a3267e64bffdf7f5
Yu-Chung Hsiao <yuchsiao@gmail.com>
2019-10-18 15:11:40 -0700

    remove files with issues

--- put.sh
+++
@@ -1,8 +0,0 @@
-rsync -arvh \
-    --exclude __pycache__ \
-    --exclude data/glove.840B.300d \
-    --exclude data/glove.840B.300d.zip \
-    --exclude emb/* \
-    --exclude emb.tar.gz \
-    --exclude save \
-    * mr85p01ad-gpu017:~/project/squad
